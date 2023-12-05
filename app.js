09119bae890709694da47100db501bf672ec29e6
Bhautik Bavadiya <hi@yesbhautik.co.in>
2023-12-05 14:20:32 +0530

    Files added

---
+++ app.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');  
+const { Resend } = require('resend');
+
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(bodyParser.json());  
+
+app.use((req, res, next) => {
+  if (req.originalUrl.endsWith('.css')) {
+    res.type('.css');
+  }
+  next();
+});
+
+app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact');
+});
+
+app.get('/404', (req, res) => {
+  res.render('404');
+});
+
+app.get('/projects/magic-app', (req, res) => {
+  res.render('projects/magic-app');
+});
+
+app.get('/projects/unbuilt', (req, res) => {
+  res.render('projects/unbuilt');
+});
+
+const resend = new Resend('re_9R4trJ8s_9Yr7PF5hrEX2XGkDZAMdBaXx');
+
+app.post('/send-email', async (req, res) => {
+  const { from, to, subject, html } = req.body;
+
+  try {
+    const { data, error } = await resend.emails.send({ from, to, subject, html });
+
+    if (error) {
+      console.error('Error sending email:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    } else {
+      // console.log('Email sent successfully:', data);
+      res.json({ success: true });
+    }
+  } catch (error) {
+    console.error('An unexpected error occurred:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+app.use((req, res) => {
+    res.status(404).render('404');
+  });
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
---
+++ public/js/script.js
@@ -0,0 +1,102 @@
+$(document).ready(function () {
+    $('.buttons-wrapper .button').on('click', function (e) {
+      e.preventDefault();
+      $('.category').removeClass('selected');
+
+      $(this).addClass('selected');
+      var category = $(this).attr('href');
+      $('.grid .project-item').show();
+      $('.grid .project-item').has('.buttons-wrapper .button[href="' + category + '"]').show();
+    });
+  });
+
+
+$(document).ready(function () {
+  var itemsToShow = 4;
+  var itemsIncrement = 2;
+  var isFiltered = false;
+  var filteredItems;
+
+  function showItems() {
+    $('.grid .project-item:lt(' + itemsToShow + ')').show();
+  }
+
+  function hideItems() {
+    $('.grid .project-item').hide();
+  }
+
+  function loadMore() {
+    itemsToShow += itemsIncrement;
+    showItems();
+    if (itemsToShow >= $('.grid .project-item').length) {
+      $('.load-more-button').hide();
+    }
+  }
+
+  function resetLoadMore() {
+    var remainingItems = filteredItems.length - itemsToShow;
+    for (var i = 0; i < remainingItems; i++) {
+      filteredItems.slice(itemsToShow, itemsToShow + itemsIncrement).show();
+      itemsToShow += itemsIncrement;
+      remainingItems = filteredItems.length - itemsToShow;
+    }
+    if (itemsToShow < filteredItems.length) {
+      $('.load-more-button').show();
+    } else {
+      $('.load-more-button').hide();
+    }
+  }
+
+  hideItems();
+  showItems();
+
+  $('.buttons-wrapper .button').on('click', function (e) {
+    e.preventDefault();
+    var category = $(this).attr('href');
+
+    itemsToShow = 4;
+    isFiltered = true;
+    hideItems();
+
+    if ($(this).hasClass('show-all')) {
+      isFiltered = false;
+
+      showItems();
+      $('.load-more-button').show();
+    } else {
+      filteredItems = $('.grid .project-item').has('.buttons-wrapper .button[href="' + category + '"]');
+      filteredItems.slice(0, itemsToShow).show();
+      $('.load-more-button').toggle(filteredItems.length > itemsToShow);
+
+      // Scroll to the filtered category
+      var scrollToElement = $('.scroll').offset().top;
+      $('html, body').animate({ scrollTop: scrollToElement }, 500);
+    }
+  });
+
+
+  $('.load-more-button').on('click', function () {
+    if (isFiltered) {
+      resetLoadMore();
+    } else {
+      loadMore();
+    }
+  });
+});
+
+  document.addEventListener('DOMContentLoaded', function () {
+  var backToTopButton = document.getElementById('back-to-top-button');
+
+  window.onscroll = function () {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      backToTopButton.style.display = 'block';
+    } else {
+      backToTopButton.style.display = 'none';
+    }
+  };
+
+  backToTopButton.addEventListener('click', function () {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0; 
+  });
+});
