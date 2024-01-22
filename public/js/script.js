e12eff05448d7cbb798a47be9907b57abac704f2
Yashvi shah <yashu.shah28@gmail.com>
2024-01-22 06:40:43 +0530

    New Code Added

--- public/js/script.js
+++ public/js/script.js
@@ -1,121 +1,169 @@
-$(document).ready(function () {
-  $(".buttons-wrapper .button").on("click", function (e) {
-    e.preventDefault();
-    $(".category").removeClass("selected");
+let mainCategories = document.querySelectorAll('.main-category');
+let subcategoryContainers = document.querySelectorAll('.subcategory-container');
+let goBackButton = document.getElementById('go-back-button'); // assuming you have a button with the id 'go-back-button'
+let subcategoryall = document.getElementById('subcategoryall'); // assuming you have a button with the id 'go-back-button'
 
-    $(this).addClass("selected");
+mainCategories.forEach((mainCategory, index) => {
+  mainCategory.addEventListener('click', () => {
+    // Check if the selected category is "All"
+    if (index === 0) {
+      // For "Show All" button, show all main categories and hide the go back button
+      document.querySelector('.buttons-wrapper').style.display = 'flex';
+      goBackButton.style.display = 'none';
 
-    if ($(this).hasClass("selected")) {
-      if ($(this).hasClass("gd")) {
-        $(".gd-subcategory").show();
-      } else {
-        $(".gd-subcategory").hide();
-      }
-      if ($(this).hasClass("wd")) {
-        $(".wd-subcategory").show();
-      } else {
-        $(".wd-subcategory").hide();
+      // Hide all subcategory containers
+      subcategoryContainers.forEach(subcategoryContainer => {
+        subcategoryContainer.style.display = 'none';
+      });
+    } else {
+      // Hide all main categories, except for "Show All"
+    mainCategories.forEach(category => {
+      category.style.display = 'none';
+    });
+      // Show the go back button
+      goBackButton.style.display = 'block';
+      $(document).ready(function () {
+ 
+        $('.all').addClass('selected');
+    });
+
+      // Hide all subcategory containers
+      subcategoryContainers.forEach(subcategoryContainer => {
+        subcategoryContainer.style.display = 'none';
+      });
+
+      // Display the selected subcategory container
+      if (index !== 0) {
+        subcategoryContainers[index - 1].style.display = 'flex';
       }
     }
-
-    var category = $(this).attr("href");
-    $(".grid .project-item").show();
-    $(".grid .project-item")
-      .has('.buttons-wrapper .button[href="' + category + '"]')
-      .show();
   });
 });
+
+goBackButton.addEventListener('click', () => {
+  // Show all main categories
+    mainCategories.forEach(category => {
+      category.style.display = 'flex';
+    });
+  // Hide the go back button
+  goBackButton.style.display = 'none';
+
+  // Hide all subcategory containers
+  subcategoryContainers.forEach(subcategoryContainer => {
+    subcategoryContainer.style.display = 'none';
+  });
+  $(document).ready(function () {
+ 
+    $('.grid .project-item').show();
+    $('.Select-category').removeClass('selected');
+    $('.show-all').addClass('selected');
+});
+});
+
+
+$(document).ready(function () {
+  $('.show-all').addClass('selected');
+  $('.buttons-wrapper .button').on('click', function (e) {
+    e.preventDefault();
+    $('.Select-category').removeClass('selected');
+
+    $(this).addClass('selected');
+    var categoryClass = $(this).attr('class').split(' ').find(c => c !== 'button' && c !== 'Select-category');
+
+    $('.grid .project-item').show();
+    $('.grid .project-item').has('.buttons-wrapper .button.' + categoryClass).show();
+  });
+});
+
+
 
 $(document).ready(function () {
   var itemsToShow = 4;
   var itemsIncrement = 2;
   var isFiltered = false;
   var filteredItems;
-
+ 
   function showItems() {
-    $(".grid .project-item:lt(" + itemsToShow + ")").show();
+     $(".grid .project-item:lt(" + itemsToShow + ")").show();
   }
-
+ 
   function hideItems() {
-    $(".grid .project-item").hide();
+     $(".grid .project-item").hide();
   }
-
+ 
   function loadMore() {
-    itemsToShow += itemsIncrement;
-    showItems();
-    if (itemsToShow >= $(".grid .project-item").length) {
-      $(".load-more-button").hide();
-    }
+     itemsToShow += itemsIncrement;
+     showItems();
+     if (itemsToShow >= $(".grid .project-item").length) {
+       $(".load-more-button").hide();
+     }
   }
-
+ 
   function resetLoadMore() {
-    var remainingItems = filteredItems.length - itemsToShow;
-    for (var i = 0; i < remainingItems; i++) {
-      filteredItems.slice(itemsToShow, itemsToShow + itemsIncrement).show();
-      itemsToShow += itemsIncrement;
-      remainingItems = filteredItems.length - itemsToShow;
-    }
-    if (itemsToShow < filteredItems.length) {
-      $(".load-more-button").show();
-    } else {
-      $(".load-more-button").hide();
-    }
+     var remainingItems = filteredItems.length - itemsToShow;
+     for (var i = 0; i < remainingItems; i++) {
+       filteredItems.slice(itemsToShow, itemsToShow + itemsIncrement).show();
+       itemsToShow += itemsIncrement;
+       remainingItems = filteredItems.length - itemsToShow;
+     }
+     if (itemsToShow < filteredItems.length) {
+       $(".load-more-button").show();
+     } else {
+       $(".load-more-button").hide();
+     }
   }
-
+ 
   hideItems();
   showItems();
-
+ 
   $(".buttons-wrapper .button").on("click", function (e) {
-    e.preventDefault();
-    var category = $(this).attr("href");
-
-    itemsToShow = 4;
-    isFiltered = true;
-    hideItems();
-
-    if ($(this).hasClass("show-all")) {
-      isFiltered = false;
-
-      showItems();
-      $(".load-more-button").show();
-    } else {
-      filteredItems = $(".grid .project-item").has(
-        '.buttons-wrapper .button[href="' + category + '"]'
-      );
-      filteredItems.slice(0, itemsToShow).show();
-      $(".load-more-button").toggle(filteredItems.length > itemsToShow);
-
-      // Scroll to the filtered category
-      var scrollToElement = $(".scroll").offset().top;
-      $("html, body").animate({ scrollTop: scrollToElement }, 500);
-    }
+     e.preventDefault();
+     var categoryClass = $(this)
+       .attr("class")
+       .split(" ")
+       .find((c) => c !== "button" && c !== "category");
+ 
+     itemsToShow = 4;
+     isFiltered = true;
+     hideItems();
+ 
+     if ($(this).hasClass("show-all")) {
+       isFiltered = false;
+       showItems();
+       $(".load-more-button").show();
+     } else {
+       filteredItems = $(".grid .project-item").has(
+         ".buttons-wrapper .button." + categoryClass
+       );
+       filteredItems.slice(0, itemsToShow).show();
+       $(".load-more-button").toggle(filteredItems.length > itemsToShow);
+ 
+       // Scroll to the filtered category
+       var scrollToElement = $(".scroll").offset().top;
+       $("html, body").animate({ scrollTop: scrollToElement }, 500);
+     }
   });
-
+ 
+  $(".button.subcategory").on("click", function (e) {
+     e.preventDefault();
+     var selectedCategory = $(this).data("category");
+ 
+     // Hide all project items
+     $(".project-item").hide();
+ 
+     // Show project items matching the selected subcategory
+     filteredItems = $(".project-item[data-category*='" + selectedCategory + "']");
+     filteredItems.slice(0, itemsToShow).show();
+     $(".load-more-button").toggle(filteredItems.length > itemsToShow);
+  });
+ 
   $(".load-more-button").on("click", function () {
-    if (isFiltered) {
-      resetLoadMore();
-    } else {
-      loadMore();
-    }
+     if (isFiltered) {
+       resetLoadMore();
+     } else {
+       loadMore();
+     }
   });
-});
+ });
 
-document.addEventListener("DOMContentLoaded", function () {
-  var backToTopButton = document.getElementById("back-to-top-button");
 
-  window.onscroll = function () {
-    if (
-      document.body.scrollTop > 20 ||
-      document.documentElement.scrollTop > 20
-    ) {
-      backToTopButton.style.display = "block";
-    } else {
-      backToTopButton.style.display = "none";
-    }
-  };
-
-  backToTopButton.addEventListener("click", function () {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-  });
-});
